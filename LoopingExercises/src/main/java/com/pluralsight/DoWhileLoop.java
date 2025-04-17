f1bcd13f7bb866a3f9375ab9aa69c344e3d92f42
InomRai <monirai849@gmail.com>
2025-04-17 13:41:10 -0400

    still learning

---
+++ LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int print = 0;
+        do {
+            System.out.println("I love java");
+            print++;
+        } while (print < 5);
+    }
+}
---
+++ LoopingExercises/src/main/java/com/pluralsight/whileLoop.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class whileLoop {
+    public static void main(String[] args) {
+
+        int print = 0;
+         while (print < 5) {
+            System.out.println("I Love java");
+            print++;
+
+
+
+        }
+    }
+}
